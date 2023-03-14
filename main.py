143f7ecb5c7bd8945541197d7624ff7678f26699
saurus <saurus.56@mail.ru>
2023-03-15 00:14:13 +0500

    15.03.23 gui

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+import sys
+
 import numpy as np
 import dataBase
 import pandas as pd
 import import_from_excel
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+from ui import Ui_MainWindow
+
+
+class MainWindows(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.start)
+        self.ui.listWidget.addItem('15')
+        self.ui.listWidget.clicked.connect(self.hdfud)
+        self.ui.action_add_edit.triggered.connect(self.gg)
+
+    def start(self):
+        print('pass')
+
+    def hdfud(self):
+        print(self.ui.listWidget.currentIndex())
+
+    def gg(self):
+        print('action')
+
+app = QApplication()
+windows = MainWindows()
+windows.show()
+sys.exit(app.exec())
 
 # dataBase.create_database()
 #
@@ -36,26 +66,26 @@ import import_from_excel
 #                            'type_': 'о'})
 
 #
-f = dataBase.Training.select(dataBase.Training.week,
-                             dataBase.Training.weight,
-                             dataBase.Training.repeat,
-                             dataBase.Training.exercise,
-                             dataBase.Exercise.muscle_target).join(dataBase.Exercise)
-
-import_from_excel.import_from_excel()
-
-
-f = pd.DataFrame(list(f.dicts()))
-# print(f)
-f['kg'] = f['repeat'] * f['weight']
-table = pd.pivot_table(f, index=['week', 'muscle_target'], aggfunc={'repeat': [np.sum, np.mean],
-                                                                    'weight': np.sum,
-                                                                    'kg': np.sum})
-print(table)
-table['mean_weight'] = table[('kg', 'sum')] / table[('repeat', 'sum')]
-table.drop(columns='weight', inplace=True)
-
-print(table.xs('Грудные', level=1))
+# f = dataBase.Training.select(dataBase.Training.week,
+#                              dataBase.Training.weight,
+#                              dataBase.Training.repeat,
+#                              dataBase.Training.exercise,
+#                              dataBase.Exercise.muscle_target).join(dataBase.Exercise)
+#
+# import_from_excel.import_from_excel()
+#
+#
+# f = pd.DataFrame(list(f.dicts()))
+# # print(f)
+# f['kg'] = f['repeat'] * f['weight']
+# table = pd.pivot_table(f, index=['week', 'muscle_target'], aggfunc={'repeat': [np.sum, np.mean],
+#                                                                     'weight': np.sum,
+#                                                                     'kg': np.sum})
+# print(table)
+# table['mean_weight'] = table[('kg', 'sum')] / table[('repeat', 'sum')]
+# table.drop(columns='weight', inplace=True)
+#
+# print(table.xs('Бицепс', level=1))
 
 
 
